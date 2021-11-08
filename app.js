c006c80bb3b3d2d6bfacd572708515bf296b6b6b
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-11-08 13:21:52 +0300

    re add json files

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ app.use(express.json());
 const movies = [
   { id: 0, name: "Pulp function", isFav: false, isDeleted: false },
   { id: 1, name: "Se7en", isFav: false, isDeleted: true },
-  { id: 2, name: "God father", isFav: false, isDeleted: true },
+  { id: 2, name: "God father", isFav: true, isDeleted: true },
   { id: 3, name: "GoodFellas", isFav: false, isDeleted: false },
-
+  { id: 4, name: "Fight club", isFav: true, isDeleted: false },
+  { id: 5, name: "Forrest gump", isFav: false, isDeleted: true },
+  { id: 6, name: "Reservior dogs", isFav: false, isDeleted: false },
+  { id: 7, name: "Casino", isFav: true, isDeleted: false },
+  { id: 8, name: "Jhon wick 2", isFav: false, isDeleted: true },
 ];
 
-// get all movies
+// Get all movies
 app.get("/get", (req, res) => {
   res.status(200);
   res.json(movies);
@@ -25,21 +29,50 @@ app.get("/get/:id", (req, res) => {
   res.status(200).json(movies[id]);
 });
 
-//creat new movie
+//Creat new movie
 app.post("/creat", (req, res) => {
   const { id, name, isFav, isDeleted } = req.body;
   movies.push({ id: movies.length, name, isFav: false, isDeleted: false });
   res.status(200).json({ id, name, isFav, isDeleted });
 });
 
-// update movie by id
+// Update movie by id
 app.put("/update/:id", (req, res) => {
   const { id, name, isFav, isDeleted } = req.body;
   movies.splice(id, 1, { id, name, isFav, isDeleted });
   res.status(200).json({ id, name, isFav, isDeleted });
 });
 
-// egt all fav movie
+// Toggle fav true/false by id
+app.put("/updaeFav/:id", (req, res) => {
+  const { id, name, isFav, isDeleted } = req.params;
+
+  if (movies[id].isDeleted == false) {
+    movies.splice(id, 1, {
+      id: movies[id].id,
+      name: movies[id].name,
+      isFav: movies[id].isFav,
+      isDeleted: true,
+    });
+  } else if (movies[id].isDeleted == true) {
+    movies.splice(id, 1, {
+      id: movies[id].id,
+      name: movies[id].name,
+      isFav: movies[id].isFav,
+      isDeleted: false,
+    });
+  }
+  res.status(200).json({ id, name, isFav, isDeleted });
+});
+
+// Show all not deleted movies
+app.get("/movies", (req, res) => {
+  let newArr = movies.filter((item) => item.isDeleted === false);
+  console.log(newArr);
+  res.status(200).json(newArr);
+});
+
+// Get all fav movies
 app.get("/favoriteMovie", (req, res) => {
   let newArr = [];
   newArr = movies.filter((item) => item.isDeleted === false);
